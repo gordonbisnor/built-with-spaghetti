2933a7abd5408a9023f9c4630c2206e312d98fdd
Gordon B. Isnor <gordonbisnor@gmail.com>
2017-05-25 00:16:03 -0300

    init

--- src/ui/components/built-with-spaghetti/component.ts
+++ src/ui/components/built-with-spaghetti/component.ts
@@ -2,4 +2,16 @@ import Component from "@glimmer/component";
 
 export default class BuiltWithSpaghetti extends Component {
 
+  items = [
+   
+    {
+      id: 1,
+      title: "zc2 gowrth of gor",
+      description: "borth of the western magnificence",
+      url: "http://vkhpulkxol.s3-website-us-east-1.amazonaws.com/",
+      img: "1.jpg"
+    }
+    
+  ];
+
 }
