6d684f59ece219d2cfe2f7b2d3e963b7e3122860
Gordon B. Isnor <gordonbisnor@gmail.com>
2017-08-23 22:22:46 -0300

    fast steve reich

--- src/ui/components/built-with-spaghetti/component.ts
+++ src/ui/components/built-with-spaghetti/component.ts
@@ -4,6 +4,15 @@ export default class BuiltWithSpaghetti extends Component {
 
   items = [
    
+       
+    {
+      id: 2,
+      title: "fast steve reich",
+      description: "so fast",
+      url: "http://fast-steve-reich.s3.amazonaws.com/index.html",
+      img: "2.png"
+    },
+    
     {
       id: 1,
       title: "zc2 gowrth of gor",
